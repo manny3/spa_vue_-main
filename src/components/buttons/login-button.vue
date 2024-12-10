9ac9c0de1f7ada05d65a47e577916e19d9b417f8
Tony <tony.chen@omnichat.ai>
2024-12-10 17:38:54 +0800

    init spa

---
+++ src/components/buttons/login-button.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <button class="button__login" @click="handleLogin">Log In</button> -->
+  <button class="button__login" @click="handleLoginV2">Log Inv2</button>
+</template>
+
+<script setup>
+import { useAuth0 } from "@auth0/auth0-vue";
+
+const { loginWithRedirect } = useAuth0();
+
+const handleLogin = () => {
+  loginWithRedirect({
+    appState: {
+      target: "/profile",
+    },
+    authorizationParams: {
+      prompt: "login",
+    },
+  });
+};
+
+const handleLoginV2 = () => {
+  // 示例：啟動登入流程的 URL
+  window.location.href = `https://${
+    import.meta.env.VITE_AUTH0_DOMAIN
+  }/authorize?response_type=code&client_id=${
+    import.meta.env.VITE_AUTH0_CLIENT_ID
+  }&redirect_uri=${import.meta.env.VITE_AUTH0_CALLBACK_URL}`;
+};
+</script>
---
+++ src/pages/callback.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1 style="color: white">callback code: {{ code }}</h1>
+</template>
+
+<script setup>
+import { ref, onMounted } from "vue";
+const code = ref("");
+
+onMounted(() => {
+  const urlParams = new URLSearchParams(window.location.search);
+  code.value = urlParams.get("code");
+});
+</script>
